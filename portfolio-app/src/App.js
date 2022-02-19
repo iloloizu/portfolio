2b77ed7c6c4842603ecba9c3a5c42aee3361cf13
Ilolo Izu <iloloizu97@gmail.com>
2022-02-19 01:21:13 -0600

    done for the night

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -3,6 +3,13 @@ import NavBar from './components/NavBar';
 import '../src/styles/App.css'
 import '../src/styles/app_layout.css'
 import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import PortfolioPage from './Pages/PortfolioPage';
+import BlogsPage from './Pages/BlogsPage';
+import ContactPage from './Pages/ContactPage';
+
+import {Routes, Route} from 'react-router-dom'
+
 
 export default function App() {
   return (
@@ -12,7 +19,18 @@ export default function App() {
       </div>
       <div className="main-content">
         <div className="content"> 
-            <HomePage/>
+          <Routes>
+              <Route path="/" element={<HomePage/>}>
+              </Route>
+              <Route path="/about" element={<AboutPage/>}>
+              </Route>
+              <Route path="/portfolio" element={<PortfolioPage/>}>
+              </Route>
+              <Route path="/blogs" element={<BlogsPage/>}>
+              </Route>
+              <Route path="/contact" element={<ContactPage/>}>
+              </Route>
+          </Routes>
           </div>
       </div>
     </div>
