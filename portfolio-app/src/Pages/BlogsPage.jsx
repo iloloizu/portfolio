3df394a462bbb38a05b563cab216d4ad6c62ea68
Ilolo Izu <iloloizu97@gmail.com>
2022-02-19 21:06:45 -0600

    finished portfolio, just need paragraphs

--- portfolio-app/src/Pages/BlogsPage.jsx
+++ portfolio-app/src/Pages/BlogsPage.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Title from '../components/Title';
+import allBlogs from "../components/allBlogs";
 
-export default function BlogsPage() {
-  return (
-    <div>BlogsPage</div>
-  )
+function BlogsPage() {
+    return (
+        <div >
+            <div className="b-title">
+                <Title title={'Recent Blogs'} span={'Recent Blogs'} />
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog)=>{
+                        return <div className="blog" key={blog.id}>
+                            <div className="blog-content">
+                                <a href={blog.link} className="blog-link">
+                                  <img src={blog.image} alt="blog-image"/>
+                                    {blog.title} 
+                                </a> <br /> {blog.month} {blog.date}, {blog.year}
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+            
+        </div>
+    )
 }
+
+export default BlogsPage;
